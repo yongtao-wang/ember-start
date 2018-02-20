ee1ad7274054e0b5d8c5661ddb60b75da9fde008
パプリカ <yongtao.in@gmail.com>
2018-02-19 16:09:51 -0800

    add model

--- app/routes/linkedin.js
+++ app/routes/linkedin.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [{
+      id: 'flagship',
+      name: 'Flagship',
+      location: 'San Francisco',
+      description: 'LinkedIn flagship website',
+    }, {
+      id: 'lts',
+      name: 'LTS',
+      location: 'Sunnyvale',
+      description: 'LinkedIn Talent Solution',
+    }, {
+      id: 'lss',
+      name: 'LSS',
+      location: 'Sunnyvale',
+      description: 'LinkedIn Sales Solution',
+    }];
+  }
 });
--- tests/acceptance/ember-start-test.js
+++ tests/acceptance/ember-start-test.js
@@ -5,8 +5,32 @@ moduleForAcceptance('Acceptance | ember start');
 
 test('visiting /', function(assert) {
   visit('/');
-
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/linkedin', 'Will Redirect automatically');
+  });
+
+});
+
+test('links to about', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'Will Show About');
+  });
+
+});
+
+test('link to contact', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'Will show Contact');
+  });
+});
+
+test('display products', function(assert){
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'Will display three items');
   });
 });
